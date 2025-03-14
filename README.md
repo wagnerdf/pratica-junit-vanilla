f1213528340749aebdb51684518204620ad8ab24
Wagner Andrade <Wagnerandrade_df@hotmail.com>
2025-03-14 12:47:50 -0300

    Implementação de 10 testes por JUnit para validar a classe

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # pratica-junit-vanilla
+
+Um financiamento possui três dados:
+
+    • totalAmont: valor total de dinheiro financiado
+    • income: renda mensal do cliente que está financiando
+    • months: número de meses do financiamento
+
+E dois métodos:
+
+    • entry: entrada do financiamento, igual a 20% do valor total
+    • quota: prestação mensal do financiamento (sem juros)
+    
+    
+Há ainda uma regra: o valor da prestação não pode ser maior que metade da renda mensal do cliente. A seguir alguns exemplos de financiamentos para ajudar a entender a regra:
+
+Exemplo 1: { totalAmount: 100000, income: 2000, months: 20 }
+Este exemplo é INVÁLIDO porque com esses dados a entrada seria 20000 e a prestação seria 4000. Porém a prestação não pode passar de 1000, que é a metade da renda do cliente.
+
+Exemplo 2: { totalAmount: 100000, income: 2000, months: 80 }
+Já este exemplo é VÁLIDO porque a entrada seria 20000 e a prestação seria 1000. Neste caso, a prestação é menor ou igual a metade da renda do cliente, satisfazendo a regra.
--- src/tests/FinancingTests.java
+++ src/tests/FinancingTests.java
@@ -1,8 +1,160 @@
 package tests;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import entities.Financing;
 
 public class FinancingTests {
+
+/* - Construtor
+      	• Deve criar o objeto com os dados corretos quando os dados forem válidos
+      	• Deve lançar IllegalArgumentException quando os dados não forem válidos
+*/
+	@Test
+	public void constructorShouldCreatedObjectWhenValidData() {
+		
+		Financing f = new Financing(100000.0, 2000.0, 80);
+		
+		Assertions.assertEquals(100000.0, f.getTotalAmount());
+		Assertions.assertEquals(2000.0, f.getIncome());
+		Assertions.assertEquals(80, f.getMonths());	
+	}
+	
+	@Test
+	public void constructorShouldThrowIllegalArgumentExceptionWhenValidData() {
+		
+		Assertions.assertThrows(IllegalArgumentException.class, () ->{
+			Financing f = new Financing(100000.0, 2000.0, 20);
+		});
+		
+	}
+	
+/* - setTotalAmount
+		• Deve atualizar o valor quando os dados forem válidos
+		• Deve lançar IllegalArgumentException quando os dados não forem válidos
+*/
+	@Test
+	public void setTotalAmountShouldSetDataWhenValidData() {
+		 
+		// arrage
+		Financing f = new Financing(100000.0, 2000.0, 80);
+		
+		// action
+		f.setTotalAmount(90000.0);
+		
+		// assert
+		Assertions.assertEquals(90000.0, f.getTotalAmount());
+
+	}
+	
+	@Test
+	public void setTotalAmountShouldThrowIllegalArgumentExceptionWhenInvalidData() {
+		
+		Assertions.assertThrows(IllegalArgumentException.class, () ->{
+			Financing f = new Financing(100000.0, 2000.0, 80);
+			f.setTotalAmount(110000.0);
+		});
+		
+	}
+	
+/* - setIncome
+ 		• Deve atualizar o valor quando os dados forem válidos
+    	• Deve lançar IllegalArgumentException quando os dados não forem válidos
+*/
+	@Test
+	public void setIncomeShouldSetDataWhenValidData() {
+		 
+		// arrage
+		Financing f = new Financing(100000.0, 2000.0, 80);
+		
+		// action
+		f.setIncome(2100.0);
+		
+		// assert
+		Assertions.assertEquals(2100.0, f.getIncome());
+
+	}
+	
+	@Test
+	public void setIncomeShouldThrowIllegalArgumentExceptionWhenInvalidData() {
+		
+		Assertions.assertThrows(IllegalArgumentException.class, () ->{
+			Financing f = new Financing(100000.0, 2000.0, 80);
+			f.setIncome(1900.0);
+		});
+		
+	}
+	
+/* - setMonths
+    	• Deve atualizar o valor quando os dados forem válidos
+    	• Deve lançar IllegalArgumentException quando os dados não forem válidos
+*/
+	@Test
+	public void setMonthsShouldSetDataWhenValidData() {
+		 
+		// arrage
+		Financing f = new Financing(100000.0, 2000.0, 80);
+		
+		// action
+		f.setMonths(81);
+		
+		// assert
+		Assertions.assertEquals(81, f.getMonths());
+
+	}
+	
+	@Test
+	public void setMonthsShouldThrowIllegalArgumentExceptionWhenInvalidData() {
+		
+		Assertions.assertThrows(IllegalArgumentException.class, () ->{
+			Financing f = new Financing(100000.0, 2000.0, 80);
+			f.setMonths(79);
+		});
+		
+	}
+
+/* - entry
+		• Deve calcular corretamente o valor da entrada
+*/
+	@Test
+	public void entryShouldCalculateEntryCorrectly() {
+		
+		Financing f = new Financing(100000.0, 2000.0, 80);
+		
+		Assertions.assertEquals(20000.0, f.entry());
+		
+	}
+	
+/* - quota
+	• Deve calcular corretamente o valor da prestação
+*/
+	@Test
+	public void quotaShouldCalculateQuotaCorrectly() {
+		
+		Financing f = new Financing(100000.0, 2000.0, 80);
+		
+		Assertions.assertEquals(1000.0, f.quota());
+		
+	}
+	
 	
 
-
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
